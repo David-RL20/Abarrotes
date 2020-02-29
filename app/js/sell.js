fa42e0e316012441692e190604ab2fdcbd1e6956
David R <alejandro20rodriguez04@gmail.com>
2020-02-28 20:19:43 -0800

    cosas comentadas pero todo funciona en sell.js

--- app/js/sell.js
+++ app/js/sell.js
@@ -1,13 +1,11 @@
-const urlProducts = 'http://localhost/Abarrotes/api/AllProducts.php'
-const urlShoppingCart='';
-// const tableBodyID='tableBody';
-const inputProduct='inputCodeProduct';
-const totalLabel='';
 
+const productsURL = 'http://localhost/Abarrotes/api/AllProducts.php';
 //Executes every time the page is refresh
 function init(){
     getProducts();
-    infoInput = document.getElementById(inputProduct);
+    console.log((sessionStorage))
+    sessionStorage.articleSelected = undefined;
+    infoInput = document.getElementById('inputCodeProduct');
     infoInput.focus();
     infoInput.addEventListener("keypress",function(){
         if(event.keyCode == '13'){
@@ -29,11 +27,12 @@ function init(){
     
 }
 
+
 //Get all the products from the data base
 function getProducts(){
     // Ask for all the products
     x= new XMLHttpRequest();
-    x.open('GET',urlProducts)
+    x.open('GET',productsURL)
     x.send()
     x.onreadystatechange = function(){
         if(x.status == 200 && x.readyState == 4){
@@ -56,10 +55,56 @@ function addProductsToDataList(products){
     });
 
 }
+
+//execute the sell, open a division for the total and type of pay 
+//also save everything in the database 
+function chargeSell(){
+    //get elements 
+    windowSell = document.getElementById('sellWindow');
+    totalLabel = document.getElementById('totalLabelWindow');
+    total = document.getElementById('totalCurrently').textContent;
+    changeLabel = document.getElementById('changeClientLabel');
+    inputAmount = document.getElementById('inputAmountPaid')
+    //inner data
+    totalLabel.innerHTML = '  '+' &#36'+' '+ parseFloat(total);
+
+    windowSell.style.display = 'block';
+    inputAmount.focus();
+
+}
+
+//Deletes an article from the table and remove its from the array
+// function deleteArticle(){
+//     if( sessionStorage.articleSelected != 'undefined'){
+//         tr =document.getElementById('row'+sessionStorage.articleSelected)
+//         tableBody = document.getElementById('tableBody')
+//         totalProduct =document.getElementById('total'+sessionStorage.articleSelected)
+//         totalAmount = document.getElementById('totalCurrently')
+        
+
+//         totalAmount.innerHTML = parseFloat(totalAmount.textContent) - parseFloat(totalProduct.textContent);
+
+//         tableBody.removeChild(tr);
+//     }
+//     else
+//     alert('Selecciona un producto para continuar');
+
+    
+// }
+
+//When you click on a Table Row(tr)
+//This method assign the value or id of this TR
+//Then you can delete it 
+// function selectArticle(id){
+//     var tr = document.getElementById(id);
+//     var tHead = document.getElementById('tableBody');
+//     tHead.removeChild(tr);
+// }
+
 //Search in database for the code of the product 
 //and add all its data 
 function addToTable(code , quantity){
-    infoInput = document.getElementById(inputProduct);
+    infoInput = document.getElementById('inputCodeProduct');
     products = JSON.parse(sessionStorage.products)
     var found = false;
     
@@ -75,7 +120,7 @@ function addToTable(code , quantity){
                 tdName = document.createElement('td')
                 tdPrice = document.createElement('td')
                 tdQuantity = document.createElement('td')
-                tdSubTotal = document.createElement('td')
+                tdTotal = document.createElement('td')
                 totalAmount = document.getElementById('totalCurrently');
                 //Set ids
                 tr.id='row'+code;
@@ -83,43 +128,53 @@ function addToTable(code , quantity){
                 tdName.id='name'+code;
                 tdPrice.id='price'+code;
                 tdQuantity.id ='quantity'+code;
-                tdSubTotal.id ='subtotal'+code;
+                tdTotal.id ='total'+code;
                 //innerHTML
                 tdCode.innerHTML = product.code;
                 tdName.innerHTML = product.name;
                 tdPrice.innerHTML= product.price;
-
-                // //set quantity 
-                // if(typeof quantity !== undefined){
-                //       tdQuantity.innerHTML = quantity;  
-                // }else{
+                // if(typeof quantity !== 'undefined'){
+                //     tdQuantity.innerHTML=quantity;
+                // }else
+                // {
                 //     tdQuantity.innerHTML=1;
                 // }
-                    
-                // //set subtotal
-                // tdSubTotal="0";
-                if(typeof quantity !== 'undefined'){
-                    tdQuantity.innerHTML=quantity;
-                }else
-                {
-                    tdQuantity.innerHTML=1;
-                }
-                tdTotal.innerHTML = (product.price * parseFloat(tdQuantity.textContent));
+                // tdTotal.innerHTML = (product.price * parseFloat(tdQuantity.textContent));
 
-                if(totalAmount.textContent != '')
-                 totalAmount.innerHTML = parseFloat(totalAmount.textContent) + parseFloat(tdTotal.textContent);
-                 else
-                 totalAmount.innerHTML = parseFloat(tdTotal.textContent);
+                // if(totalAmount.textContent != '')
+                //  totalAmount.innerHTML = parseFloat(totalAmount.textContent) + parseFloat(tdTotal.textContent);
+                //  else
+                //  totalAmount.innerHTML = parseFloat(tdTotal.textContent);
 
-                //send to server session 
+                 //onclick event
 
+                //  tr.onclick = function(){
+                //      if( sessionStorage.articleSelected != 'undefined'){
+                //          try {
+                //              tableBefore = document.getElementById('row'+sessionStorage.articleSelected)
+                //              tableBefore.style.background = '#FFF';
+                             
+                //          } catch (error) {
+                //              console.log('Articulo fue eliminado');
+                //          }
+                //          sessionStorage.articleSelected = product.code;
+                //          tableRow = document.getElementById('row'+product.code)
+                //          tableRow.style.background = '#ccc'
+                //      }else{
+                //         sessionStorage.articleSelected = product.code;
+                //         tableRow = document.getElementById('row'+product.code)
+                //         tableRow.style.background = '#ccc'
+                //      }
+                         
+                     
+                //  }
 
                 //Append everthing
                 tr.appendChild(tdCode);
                 tr.appendChild(tdName);
                 tr.appendChild(tdPrice);
                 tr.appendChild(tdQuantity);
-                tr.appendChild(tdSubTotal);
+                tr.appendChild(tdTotal);
                 tableBody.appendChild(tr);
             }
         }
@@ -127,13 +182,21 @@ function addToTable(code , quantity){
 
     if(!found){
         alert('Producto invalido');
-        resetInput();
+        infoInput.value='';
+        infoInput.focus();
         
     }
     console.log(found + ' ' + code);
 }
-//verify if there is a  * in the input products so it can increase the quantity
+
+//Hide the window sell from content
+function exitBuyWindow (){
+    windowSell = document.getElementById('sellWindow');
+    windowSell.style.display = 'none'
+}
+
 function moreProducts(text){
+    var stringInput = document.getElementById('inputCodeProduct')
     string = text;
     position = string.indexOf("*");
     if(position != -1){
@@ -141,12 +204,14 @@ function moreProducts(text){
         quantity = string.slice(0,position);
         if(quantity.match(/[a-z]/i)){
             alert('Cantidad equivocada');
-            resetInput();
+            stringInput.value=''
+            stringInput.focus();
         }
         productCode = string.slice(position + 1,string.length)
         if(productCode.match(/[a-z]/i)){
             alert('El codigo del producto no deber contener letras');
-            resetInput();
+            stringInput.value=''
+            stringInput.focus();
         }
         error = false;
         //validates if it has more than one *
@@ -157,7 +222,8 @@ function moreProducts(text){
         }
         if(error){
             alert('Error De Cantidad, Demasiados ** ')
-            resetInput();
+            stringInput.value=''
+            stringInput.focus();
         }
         return Array(quantity,productCode) 
     }
@@ -165,8 +231,6 @@ function moreProducts(text){
         return undefined
 }
 
-//verify if the 
-//returns true if found and false if not
 function verifyIsInTable(code , quantity){
     found = false;
     try {
@@ -175,16 +239,18 @@ function verifyIsInTable(code , quantity){
         tdPrice = document.getElementById('price'+code);
         tdTotal = document.getElementById('total'+code);
         totalAmount =document.getElementById('totalCurrently');
-         //get subtotal's product
+        if(totalAmount.textContent !='')
+        totalCurrent = parseFloat(totalAmount.textContent) - (parseFloat(tdQuantity.textContent) * parseFloat(tdPrice.textContent));
+        if(typeof quantity !== 'undefined')
+         totalProduct = parseFloat(tdQuantity.textContent) + parseFloat(quantity);
+         else
+         totalProduct = parseFloat(tdQuantity.textContent) + 1;
 
-        //update total's
+    
+        totalAmount.innerHTML = totalCurrent + (totalProduct * parseFloat(tdPrice.textContent));
+        tdQuantity.innerHTML = totalProduct;
+        tdTotal.innerHTML = totalProduct * parseFloat(tdPrice.textContent);
 
-        //if quantity is defined update
-        if(typeof quantity !== 'undefined'){
-
-        } else{
-
-        }
         found=true;
     } catch (error) {
         found = false;
@@ -193,83 +259,19 @@ function verifyIsInTable(code , quantity){
     return found;
 }
 
-//Asign an empty value to product's input
-//and focuses it
-function resetInput(){
-    var stringInput = document.getElementById(inputProduct)
-    stringInput.value=''
-    stringInput.focus();
+// function inputPaid(){
+//     var input = document.getElementById('inputAmountPaid');
 
-}
-
-//Hide the window sell from content
-function exitBuyWindow (){
-    windowSell = document.getElementById('sellWindow');
-    windowSell.style.display = 'none'
-}
-//execute the sell, open a division for the total and type of pay 
-//also save everything in the database 
-function chargeSell(){
-    //get elements 
-    windowSell = document.getElementById('sellWindow');
-    totalLabel = document.getElementById('totalLabelWindow');
-    total = document.getElementById('totalCurrently').textContent;
-    changeLabel = document.getElementById('changeClientLabel');
-    inputAmount = document.getElementById('inputAmountPaid')
-    //inner data
-    totalLabel.innerHTML = '  '+' &#36'+' '+ parseFloat(total);
-
-    windowSell.style.display = 'block';
-    inputAmount.focus();
-}
-
-// Deletes an article from the table and remove its from the array
-function deleteArticle(){
-    if( sessionStorage.articleSelected != 'undefined'){
-        tr =document.getElementById('row'+sessionStorage.articleSelected)
-        tableBodyID = document.getElementById('tableBody')
-        totalProduct =document.getElementById('total'+sessionStorage.articleSelected)
-        totalAmount = document.getElementById('totalCurrently')
-        
-
-        totalAmount.innerHTML = parseFloat(totalAmount.textContent) - parseFloat(totalProduct.textContent);
-
-        tableBodyID.removeChild(tr);
-    }
-    else
-    alert('Selecciona un producto para continuar');
-
-    
-}
-
-// When you click on a Table Row(tr)
-// This method assign the value or id of this TR
-// Then you can delete it 
-function selectArticle(id){
-    var tr = document.getElementById(id);
-    var tHead = document.getElementById('tableBody');
-    tHead.removeChild(tr);
-}
-
-
-
-
-
-
-
-function inputPaid(){
-    var input = document.getElementById('inputAmountPaid');
-
-    //event listener
-    input.addEventListener('keyup', function(){
-        if(event.keyCode == '13'){
-            console.log('u paining');
-        }
-        if(event.keyCode == '27'){
-            windowSell = document.getElementById('sellWindow');
-            inputProduct = document.getElementById(inputProduct);
-            windowSell.style.display = 'none';
-            inputProduct.focus();
-        }
-    });
-}
+//     //event listener
+//     input.addEventListener('keyup', function(){
+//         if(event.keyCode == '13'){
+//             console.log('u paining');
+//         }
+//         if(event.keyCode == '27'){
+//             windowSell = document.getElementById('sellWindow');
+//             inputProduct = document.getElementById('inputCodeProduct');
+//             windowSell.style.display = 'none';
+//             inputProduct.focus();
+//         }
+//     });
+// }
